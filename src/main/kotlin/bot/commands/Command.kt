7a2bd2a8e748e03127cccf1c7f2147b7c05fd53f
Tim Castelijns <timcastelijns@gmail.com>
2018-07-31 22:11:02 +0200

    Add some restrictive variance to command interfaces

--- src/main/kotlin/bot/commands/Command.kt
+++ src/main/kotlin/bot/commands/Command.kt
@@ -4,10 +4,10 @@ import io.reactivex.Completable
 import io.reactivex.Single
 
 
-interface Command<P, T> {
+interface Command<in P, out T> {
 
     fun execute(params: P): T
 }
 
-interface CompletableCommand<P> : Command<P, Completable>
-interface SingleCommand<P, T> : Command<P, Single<T>>
+interface CompletableCommand<in P> : Command<P, Completable>
+interface SingleCommand<in P, T> : Command<P, Single<T>>
