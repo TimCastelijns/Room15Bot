1016d95e3ba150b53fabebe8962b15efbf5c430c
Tim Castelijns <timcastelijns@gmail.com>
2019-01-11 16:28:23 +0100

    Add user profiles table

--- src/main/kotlin/com/timcastelijns/room15bot/data/db/Schema.kt
+++ src/main/kotlin/com/timcastelijns/room15bot/data/db/Schema.kt
@@ -15,3 +15,13 @@ object Reminders : IntIdTable() {
     val triggerAt = long("triggerAt")
     val completed = bool("completed").default(false)
 }
+
+object UserProfiles: IntIdTable() {
+    val userId = integer("userId")
+    val username = varchar("username", 255)
+    val nickname = varchar("nickname", 255)
+    val age = integer("age")
+    val location = varchar("location", 255)
+    val skills = varchar("skills", 255)
+    val title = varchar("title", 1024)
+}
